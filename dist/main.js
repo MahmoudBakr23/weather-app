c49c540c948f1e84361482680fdb8a2ef6139a3b
Mahmoud Bakr <mbakr6821@gmail.com>
2020-12-20 07:09:06 +0200

    setup the form to send and receive data correctly

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,24 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-console.log("let's go!");
+const searchForm = document.getElementById('search')
+const searchInput = document.getElementById('search-word')
+const searchBtn = document.getElementById('search-btn')
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const wApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=15f1928a4dbbfb07a75f35e6dd48bc2c`
+
+    async function getWeather() {
+        const response = await fetch(wApi, {mode: 'cors'})
+        const myData = await response.json();
+        console.log(myData)
+    }
+    getWeather()
+
+    searchForm.reset();
+})
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
